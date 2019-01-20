cdbf507980fd710df10f7715b135407d11127ef6
arantxagea <arantxagea@gmail.com>
2019-01-20 12:46:03 +1100

    implement generic case

--- src/fibonacci.py
+++ src/fibonacci.py
@@ -1,3 +1,7 @@
 def compute_fibonacci(n):
-    return 0
-io
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    else:
+        return compute_fibonacci(n-1) + compute_fibonacci(n-2)
--- tests/test_fibonacci.py
+++ tests/test_fibonacci.py
@@ -3,3 +3,7 @@ from src.fibonacci import compute_fibonacci
 
 def test_fibonacci_0_is_0():
     assert 0 == compute_fibonacci(0)
+
+
+def test_fibonacci_1_is_1():
+    assert 1 == compute_fibonacci(1)
